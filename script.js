d079b812ae366c59d8ef9df68c0fac9f498afbf9
mountaindew2141@outlook.com <mountaindew2141@outlook.com>
2021-02-20 02:42:07 -0500

    updated JS file

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 var str = "Hello Developers!"; 
 console.log(str); 
+
+
+var studentName;
+var studentName = "Placeholder";
+var studentAge = 32;
+
+studentName = "Here";
+studentAge = 52;
+
+console.log(studentName);
+console.log("is my ");
+
+studentName="my"
+
+var studentName;
+
+console.log("portfolio!");
